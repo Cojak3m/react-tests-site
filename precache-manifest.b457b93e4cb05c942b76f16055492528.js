6ceb24a8efae1e325e4f273f4288ec5e1052b315
Cojak3m <cojak3m@gmail.com>
2020-08-13 19:04:22 +0200

    Updates

---
+++ precache-manifest.b457b93e4cb05c942b76f16055492528.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e695b79c4556ecaa9a4adbec4df075",
+    "url": "/react-tests-site/index.html"
+  },
+  {
+    "revision": "993b5393d0c9889deaa7",
+    "url": "/react-tests-site/static/css/main.45ce1fdf.chunk.css"
+  },
+  {
+    "revision": "5630e3687dd76ec22ab5",
+    "url": "/react-tests-site/static/js/2.22fd7bd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tests-site/static/js/2.22fd7bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "993b5393d0c9889deaa7",
+    "url": "/react-tests-site/static/js/main.e05d19a0.chunk.js"
+  },
+  {
+    "revision": "ffa387fb8924b4d2fc9e",
+    "url": "/react-tests-site/static/js/runtime-main.7482b375.js"
+  }
+]);
